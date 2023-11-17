6396fb21596e7a5ebf364eca93ca05efab5c3bba
devKeed <4tuneadebiyi@gmail.com>
2023-11-17 19:55:29 +0100

    reaarange

---
+++ my-app/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Header from '../public/Components/Header/Header'
+import Hero from '../public/Components/Hero/Hero'
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Header/>
+     <Hero/>
+    </>
+  )
+}
+
+export default App
